f37c2939c1cf6f2fad3dbcde133ebf415e6f8be3
Arash Farahmandi <farahmandi.arash@gmail.com>
2023-04-04 11:09:06 +0330

    1402-01-15 fix services

--- WebApi/StartupConfiguration/LogConfiguration.cs
+++ WebApi/StartupConfiguration/LogConfiguration.cs
@@ -4,11 +4,8 @@ public static  class LogConfiguration
 {
     public static void Configuration(WebApplicationBuilder builder)
     {
-        builder.Services.AddScheduler();
-
         builder.Logging.ClearProviders();
         builder.Logging.AddConsole();
         builder.Host.UseSerilog((context, loggerConfiguration) => loggerConfiguration.ReadFrom.Configuration(context.Configuration));
-
     }
 }
---
+++ WebApi/StartupConfiguration/ServiceConfiguration.cs
@@ -0,0 +1,19 @@
+﻿namespace WebApi.StartupConfiguration;
+
+public static  class ServiceConfiguration
+{
+    public static void Configuration(IServiceCollection collection)
+    {
+        // collection.UseScheduler(scheduler =>
+        // {
+        //     scheduler.Schedule<ScheduleDatabaseInvocable>()
+        //     .DailyAtHour(4);
+        // });
+        collection.AddApplication();
+
+        collection.AddHealthChecks();
+        
+        collection.AddScheduler();
+
+    }
+}
