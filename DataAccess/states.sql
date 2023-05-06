77f8821d0f5ee0451ec56d3225072303b086d33b
Arash Farahmandi <farahmandi.arash@gmail.com>
2023-05-06 12:29:12 +0330

    1402-02-16 add city

---
+++ DataAccess/states.sql
@@ -0,0 +1,60 @@
+INSERT Into public."States" ("Id", "Code", "Title") VALUES (16, 44, N'آذربايجان غربي')
+;
+INSERT Into public."States" ("Id", "Code", "Title") VALUES (17, 45, N'اردبيل')
+;
+INSERT Into public."States" ("Id", "Code", "Title") VALUES (18, 31, N'اصفهان')
+;
+INSERT Into public."States" ("Id", "Code", "Title") VALUES (19, 84, N'ايلام')
+;
+INSERT Into public."States" ("Id", "Code", "Title") VALUES (20, 77, N'بوشهر')
+;
+INSERT Into public."States" ("Id", "Code", "Title") VALUES (21, 26, N'البرز')
+;
+INSERT Into public."States" ("Id", "Code", "Title") VALUES (22, 21, N'تهران')
+;
+INSERT Into public."States" ("Id", "Code", "Title") VALUES (23, 38, N'چهارمحال بختياري')
+;
+INSERT Into public."States" ("Id", "Code", "Title") VALUES (24, 56, N'خراسان جنوبي')
+;
+INSERT Into public."States" ("Id", "Code", "Title") VALUES (25, 51, N'خراسان رضوي')
+;
+INSERT Into public."States" ("Id", "Code", "Title") VALUES (26, 58, N'خراسان شمالي')
+;
+INSERT Into public."States" ("Id", "Code", "Title") VALUES (27, 61, N'خوزستان')
+;
+INSERT Into public."States" ("Id", "Code", "Title") VALUES (28, 24, N'زنجان')
+;
+INSERT Into public."States" ("Id", "Code", "Title") VALUES (29, 23, N'سمنان')
+;
+INSERT Into public."States" ("Id", "Code", "Title") VALUES (30, 54, N'سيستان و بلوچستان')
+;
+INSERT Into public."States" ("Id", "Code", "Title") VALUES (31, 71, N'فارس')
+;
+INSERT Into public."States" ("Id", "Code", "Title") VALUES (32, 28, N'قزوين')
+;
+INSERT Into public."States" ("Id", "Code", "Title") VALUES (33, 25, N'قم')
+;
+INSERT Into public."States" ("Id", "Code", "Title") VALUES (34, 87, N'كردستان')
+;
+INSERT Into public."States" ("Id", "Code", "Title") VALUES (35, 34, N'كرمان')
+;
+INSERT Into public."States" ("Id", "Code", "Title") VALUES (36, 83, N'كرمانشاه')
+;
+INSERT Into public."States" ("Id", "Code", "Title") VALUES (37, 74, N'كهكيلويه و بويراحمد')
+;
+INSERT Into public."States" ("Id", "Code", "Title") VALUES (38, 17, N'گلستان')
+;
+INSERT Into public."States" ("Id", "Code", "Title") VALUES (39, 13, N'گيلان')
+;
+INSERT Into public."States" ("Id", "Code", "Title") VALUES (40, 66, N'لرستان')
+;
+INSERT Into public."States" ("Id", "Code", "Title") VALUES (41, 15, N'مازندران')
+;
+INSERT Into public."States" ("Id", "Code", "Title") VALUES (42, 86, N'مركزي')
+;
+INSERT Into public."States" ("Id", "Code", "Title") VALUES (43, 76, N'هرمزگان')
+;
+INSERT Into public."States" ("Id", "Code", "Title") VALUES (44, 81, N'همدان')
+;
+INSERT Into public."States" ("Id", "Code", "Title") VALUES (45, 35, N'يزد')
+;
