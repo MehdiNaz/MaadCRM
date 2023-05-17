215c3bbcba15a8e6cc84e88cc8526f74666a7305
Arash Farahmandi <farahmandi.arash@gmail.com>
2023-05-17 17:15:44 +0330

    fix

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -31,11 +31,11 @@ app.UseCors();
 
 app.UseAuthorization();
 
-if (app.Environment.IsDevelopment())
-{
+// if (app.Environment.IsDevelopment())
+// {
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+// }
 
 app.UseHttpsRedirection();
 
